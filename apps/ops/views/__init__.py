5664506c8d5de707dd11d0c81cd7a6f9dd930bdd
hoke58 <hoke58@qq.com>
2018-12-14 23:23:00 +0800

    process

--- apps/ops/views/__init__.py
+++ apps/ops/views/__init__.py
@@ -1,3 +1,4 @@
 from .adhoc import *
 from .celery import *
 from .command import *
+from .process import *
---
+++ apps/ops/views/process.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+
+from django.utils.translation import ugettext as _
+from django.conf import settings
+from django.views.generic import ListView, DetailView, TemplateView, View
+from common.mixins import DatetimeSearchMixin
+from .models import Task, AdHoc, AdHocRunHistory, CeleryTask
+from django.contrib.auth.mixins import LoginRequiredMixin
+from common.permissions import SuperUserRequiredMixin, AdminUserRequiredMixin
+
+__all__ = [
+    'ProcessContrlView'
+]
+
+
+class ProcessContrlView(LoginRequiredMixin, TemplateView):
+    template_name = 'ops/process.html'
+
+    def get_context_data(self, **kwargs):
+        context = {
+            'app': _('Ops'),
+            'action': _('Process Control'),
+            'src_url': settings.SUPERVISORD_URL,
+        }
+        kwargs.update(context)
+        return super().get_context_data(**kwargs)
